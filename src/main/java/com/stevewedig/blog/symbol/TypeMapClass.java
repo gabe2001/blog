ac2ea934165d2dc64dbbf655f33c53c43b772fd2
gabe2001 <gabe@hpe.com>
2021-05-23 11:43:16 +0200

    Update TypeMapClass.java

--- src/main/java/com/stevewedig/blog/symbol/TypeMapClass.java
+++ src/main/java/com/stevewedig/blog/symbol/TypeMapClass.java
@@ -1,101 +1,109 @@
 package com.stevewedig.blog.symbol;
 
-import java.util.*;
-
 import com.google.common.collect.Maps;
 import com.stevewedig.blog.errors.NotContained;
 import com.stevewedig.blog.util.CastLib;
 import com.stevewedig.blog.value_objects.EntityMixin;
 
-class TypeMapClass extends EntityMixin implements TypeMap {
+import java.util.HashMap;
+import java.util.Map;
 
-  // ===========================================================================
-  // state
-  // ===========================================================================
+class TypeMapClass extends EntityMixin implements TypeMap
+{
 
-  private final Map<Class<?>, Object> state;
+   // ===========================================================================
+   // state
+   // ===========================================================================
 
-  @Override
-  public Map<Class<?>, Object> stateCopy() {
-    return Maps.newHashMap(state);
-  }
-  
-  @Override
-  protected Object[] fields() {
-    Object[] fields = new Object[state.size() * 2];
+   private final Map<Class<?>, Object> state;
 
-    int i = 0;
-    for (Class<?> type : state.keySet()) {
-      Object value = state.get(type);
+   public TypeMapClass()
+   {
+      this.state = new HashMap<>();
+   }
 
-      fields[i] = type;
-      fields[i + 1] = value;
+   public TypeMapClass(Map<Class<?>, Object> state)
+   {
+      this.state = Maps.newHashMap(state); // defensive copy
+   }
 
-      i += 2;
-    }
+   // ===========================================================================
+   // constructor
+   // ===========================================================================
 
-    return fields;
-  }
-  
-  // ===========================================================================
-  // constructor
-  // ===========================================================================
+   @Override
+   public Map<Class<?>, Object> stateCopy()
+   {
+      return Maps.newHashMap(state);
+   }
 
-  public TypeMapClass() {
-    this.state = new HashMap<>();
-  }
+   @Override
+   protected Object[] fields()
+   {
+      Object[] fields = new Object[state.size() * 2];
+      int i = 0;
+      for (Map.Entry<Class<?>, Object> entry : state.entrySet())
+      {
+         fields[i++] = entry.getKey();
+         fields[i++] = entry.getValue();
+      }
+      return fields;
+   }
 
-  public TypeMapClass(Map<Class<?>, Object> state) {
-    this.state = Maps.newHashMap(state); // defensive copy
-  }
+   // ===========================================================================
+   // core behavior
+   // ===========================================================================
 
-  // ===========================================================================
-  // core behavior
-  // ===========================================================================
+   @Override
+   public <T> TypeMap put(Class<T> type, T value)
+   {
+      state.put(type, value);
+      return this; // fluent
+   }
 
-  @Override
-  public <Value> TypeMap put(Class<Value> type, Value value) {
-    state.put(type, value);
-    return this; // fluent
-  };
+   @Override
+   public <T> T get(Class<T> type)
+   {
 
-  @Override
-  public <Value> Value get(Class<Value> type) {
+      if (!containsKey(type))
+         throw new NotContained("type = %s", type);
 
-    if (!containsKey(type))
-      throw new NotContained("type = %s", type);
+      // this is type safe as long the entry was added via ClassMap.put()
+      return CastLib.get(state, type);
+   }
 
-    // this is type safe as long the entry was added via ClassMap.put()
-    return CastLib.get(state, type);
-  }
+   // ===========================================================================
+   // other map behavior
+   // ===========================================================================
 
-  // ===========================================================================
-  // other map behavior
-  // ===========================================================================
+   @Override
+   public boolean containsKey(Class<?> type)
+   {
+      return state.containsKey(type);
+   }
 
-  @Override
-  public boolean containsKey(Class<?> type) {
-    return state.containsKey(type);
-  }
+   @Override
+   public void remove(Class<Integer> type)
+   {
+      state.remove(type);
+   }
 
-  @Override
-  public void remove(Class<Integer> type) {
-    state.remove(type);
-  }
+   @Override
+   public boolean isEmpty()
+   {
+      return state.isEmpty();
+   }
 
-  @Override
-  public boolean isEmpty() {
-    return state.isEmpty();
-  }
+   @Override
+   public void clear()
+   {
+      state.clear();
+   }
 
-  @Override
-  public void clear() {
-    state.clear();
-  }
-
-  @Override
-  public int size() {
-    return state.size();
-  }
+   @Override
+   public int size()
+   {
+      return state.size();
+   }
 
 }
